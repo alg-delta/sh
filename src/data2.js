370c7d8da0e09100918cc1d880dee579203fde29
LarysaHliieva <lora.glievaya@gmail.com>
2024-05-28 14:34:32 +0300

    fix

--- src/data2.js
+++ src/data2.js
@@ -13,5 +13,19 @@ const data = [
     image: "moto2.jpg",
     text: "",
   },
+  {
+    id: 20,
+    name: "Ducati Panigale Superleggera V4",
+    price: "150000",
+    image: "moto1.png",
+    text: "Побудований для досвідченого постійного клієнта Benetti, який тепер переїжджає на новий 50-метровий сталевий корпус Benetti",
+  },
+  {
+    id: 21,
+    name: "Kawasaki H2R",
+    price: "75000",
+    image: "moto2.jpg",
+    text: "",
+  },
 ];
 export default data;
